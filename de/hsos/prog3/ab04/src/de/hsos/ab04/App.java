116c7f0f5dffcbc637c8b8d76457e9daa161c58f
pjd <pjd-popos@pop-os.localdomain>
2020-11-22 21:05:33 +0100

    app

--- de/hsos/prog3/ab04/src/de/hsos/ab04/App.java
+++ de/hsos/prog3/ab04/src/de/hsos/ab04/App.java
@@ -2,6 +2,8 @@ package de.hsos.ab04;
 
 import de.hsos.ab04.aufg1.Ball;
 import de.hsos.ab04.aufg1.Korb;
+import de.hsos.ab04.aufg2.Spieler;
+import de.hsos.ab04.aufg2.Spielfeld;
 import de.hsos.ab04.util.Interaktionsbrett;
 
 public class App {
@@ -11,13 +13,22 @@ public class App {
 
         Interaktionsbrett ib = new Interaktionsbrett();
 
+        Spielfeld s = new Spielfeld();
+        Spieler spieler1 = new Spieler(s,10,0);
+        Spieler spieler2 = new Spieler(s, 100,0);
+        s.darstellen(ib);
+        spieler1.DrawSchlaeger(ib);
+        spieler2.DrawSchlaeger(ib);
 
+
+
+        /*
         Ball b = new Ball();
         Korb k = new Korb();
         b.darstellen(ib);
         k.darstellen(ib);
 
-
+*/
 
 
     }
