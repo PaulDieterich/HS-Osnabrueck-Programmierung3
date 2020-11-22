4c7fbd7a571f0e346bd28ecc8a7c6e8e8fb1941f
pjd <pjd-popos@pop-os.localdomain>
2020-11-22 21:07:17 +0100

    implement Spieler

--- de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/Spieler.java
+++ de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/Spieler.java
@@ -1,4 +1,41 @@
 package de.hsos.ab04.aufg2;
 
+import de.hsos.ab04.util.Interaktionsbrett;
+
 public class Spieler {
+
+    Spielfeld spielfeld;
+    Rechteck schlaeger;
+    int punkte;
+
+    public Spieler(Spielfeld m_spielfeld, int m_x, int my_y){
+        spielfeld = m_spielfeld;
+        schlaeger = new Rechteck(SchlaegerBreiteBerechnen(), SchlaegerHoeheBerechnen());
+    }
+    public int SchlaegerHoeheBerechnen(){
+        return spielfeld.spielfeldflaeche.getHoehe() / 10;
+    }
+    public int SchlaegerBreiteBerechnen(){
+        return spielfeld.spielfeldflaeche.getBreite() / 100;
+    }
+    public void aufwaerts(){
+        if(schlaeger.oben() < spielfeld.getSpielfeldflaeche().oben()){
+            schlaeger.verschiebeNach(0,5);
+        }
+    }
+    public void abwaerts(){
+        if(schlaeger.unten() < spielfeld.getSpielfeldflaeche().unten()){
+            schlaeger.verschiebeNach(0,-5);
+        }
+    }
+    public  void erhoehePunkte(){
+        punkte++;
+    }
+    public  void setzePunkteZurueck(){
+        punkte = 0;
+    }
+    public void DrawSchlaeger(Interaktionsbrett ib){
+        schlaeger.darstellenFuellung(ib);
+    }
+
 }
