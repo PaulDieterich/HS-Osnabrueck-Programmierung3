7e86b52ab34aec015391249717743b38e92c41ae
pjd <pjd-popos@pop-os.localdomain>
2020-11-22 21:25:01 +0100

    starting implementation

--- de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/PongSpiel.java
+++ de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/PongSpiel.java
@@ -1,4 +1,15 @@
 package de.hsos.ab04.aufg2;
 
+import de.hsos.ab04.util.Interaktionsbrett;
+
 public class PongSpiel {
+
+    Spielfeld spielfeld;
+
+    Spieler spielerLinks;
+    Spieler spielerRechts;
+
+    Interaktionsbrett ib;
+
+    
 }
