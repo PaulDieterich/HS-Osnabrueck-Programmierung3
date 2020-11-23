7582c6d471107cf0c7e46c268be57362736ce2c0
pjd <pjd-popos@pop-os.localdomain>
2020-11-23 12:43:37 +0100

    ball implementierung

--- de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/Ball.java
+++ de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/Ball.java
@@ -1,4 +1,25 @@
 package de.hsos.ab04.aufg2;
 
 public class Ball {
+
+    Rechteck form;
+    int bewegungInXProFrame = 4;
+    int bewegungInYProFrame = 1;
+
+    Ball(Rechteck m_form, int m_bewegungInXProFrame, int m_bewegungInYProFrame){
+        form = m_form;
+        bewegungInXProFrame = m_bewegungInXProFrame;
+        bewegungInYProFrame = m_bewegungInYProFrame;
+    }
+    Ball(Rechteck m_form){
+        form = m_form;
+    }
+
+    public void bewegen(int anzahlFrames){
+        form.verschiebe(bewegungInXProFrame * anzahlFrames, bewegungInYProFrame * anzahlFrames);
+    }
+    
+
+
+
 }
