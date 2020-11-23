99c1948418bb0133ec95089b3238da1e63263684
pjd <pjd-popos@pop-os.localdomain>
2020-11-23 11:14:01 +0100

    PongSpiel spielen() implementierung

--- de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/PongSpiel.java
+++ de/hsos/prog3/ab04/src/de/hsos/ab04/aufg2/PongSpiel.java
@@ -3,7 +3,7 @@ package de.hsos.ab04.aufg2;
 import de.hsos.ab04.util.Interaktionsbrett;
 
 public class PongSpiel {
-
+    final int FPMS = 17;
     Spielfeld spielfeld;
 
     Spieler spielerLinks;
@@ -11,5 +11,31 @@ public class PongSpiel {
 
     Interaktionsbrett ib;
 
+
+    PongSpiel(){
+        ib = new Interaktionsbrett();
+        ib.willTasteninfo(this);
+        startAustellung();
+    }
+
+
+    private void startAustellung(){
+        spielfeld = new Spielfeld();
+        spielerLinks = new Spieler(spielfeld,spielfeld.spielfeldflaeche.mitteInX(),spielerLinks.schlaeger.getBreite());
+        spielerRechts = new Spieler(spielfeld, spielfeld.spielfeldflaeche.mitteInX(), spielfeld.spielfeldflaeche.getBreite() - spielerRechts.schlaeger.getBreite());
+    }
+    public void spielen(){
+        int differenz = 0;
+        while (true){
+            //Thread.sleep();
+            ib.abwischen();
+            spielfeld.darstellen(ib);
+            spielerLinks.DrawSchlaeger(ib);
+            spielerRechts.DrawSchlaeger(ib);
+
+
+
+        }
+    }
     
 }
