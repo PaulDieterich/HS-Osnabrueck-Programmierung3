44f0c73fd8f6822fda58302ff13d6f3b2977cb62
pjd <pjd-popos@pop-os.localdomain>
2020-11-23 14:29:55 +0100

    anpassung nach aufgabe2.6

--- de/hsos/prog3/ab04/src/de/hsos/ab04/App.java
+++ de/hsos/prog3/ab04/src/de/hsos/ab04/App.java
@@ -1,26 +1,19 @@
 package de.hsos.ab04;
 
+import de.hsos.ab04.util.Interaktionsbrett;
+//fuer aufgabe1
 import de.hsos.ab04.aufg1.Ball;
 import de.hsos.ab04.aufg1.Korb;
-import de.hsos.ab04.aufg2.Spieler;
-import de.hsos.ab04.aufg2.Spielfeld;
-import de.hsos.ab04.util.Interaktionsbrett;
+//fuer aufgabe2
+import de.hsos.ab04.aufg2.PongSpiel;
 
 public class App {
 
     public static void main(String[] args) {
 	// write your code here
 
-        Interaktionsbrett ib = new Interaktionsbrett();
-
-        Spielfeld s = new Spielfeld();
-        Spieler spieler1 = new Spieler(s,10,0);
-        Spieler spieler2 = new Spieler(s, 100,0);
-        s.darstellen(ib);
-        spieler1.DrawSchlaeger(ib);
-        spieler2.DrawSchlaeger(ib);
-
-
+        PongSpiel spiel = new PongSpiel();
+        spiel.spielen();
 
         /*
         Ball b = new Ball();
